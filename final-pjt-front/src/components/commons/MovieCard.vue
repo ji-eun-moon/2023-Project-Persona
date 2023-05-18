2b5dc541df9b0a50504bef04b18550b65c6e8506
문지은 <mjieun0956@gmail.com>
2023-05-18 14:08:39 +0900

    trendingList 기능 구현

--- final-pjt-front/src/components/commons/MovieCard.vue
+++ final-pjt-front/src/components/commons/MovieCard.vue
@@ -1,15 +1,76 @@
 <template>
-  <div>
-    <h1>MovieCard</h1>
+  <div class="card h-100 shadow" @mouseover="hover = true" @mouseleave="hover = false">
+    <div class="image-wrapper">
+      <img :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" alt="poster">
+      <div class="card-info" :class="{ 'hovered': hover }">
+        <h5 class="card-title text-center" v-show="hover">{{ movie.title }}</h5>
+        <div class="icon-text justify-content-end" v-show="hover">
+          <i class="bi bi-star-fill c-yellow me-2"></i>
+          <p class="content c-yellow">{{ movie.vote_average }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
     name: "MovieCard",
+    props: {
+      movie: Object
+    },
+    data() {
+    return {
+      hover: false
+    }
+  }
 }
 </script>
 
 <style>
+.card {
+  position: relative;
+  cursor: pointer;
+}
 
+.image-wrapper {
+  position: relative;
+}
+
+.card-info {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: #fff;
+  padding: 10px;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.card-info.hovered {
+  opacity: 1;
+}
+
+.card-title {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 700;
+  margin: 0;
+  text-align: center;
+}
+
+.content {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 600;
+  margin: 0;
+}
+
+.icon-text {
+  display: flex;
+}
+
+.c-yellow {
+  color: #FBBF24;
+}
 </style>
--- final-pjt-front/src/components/movies/TrendingList.vue
+++ final-pjt-front/src/components/movies/TrendingList.vue
@@ -1,15 +1,104 @@
 <template>
   <div>
     <h5>TrendingList</h5>
+    <button @click="showDailyTrendingMovies">일별</button>
+    <button @click="showWeeklyTrendingMovies">주별</button>
+    <h2 v-if="showDaily">Daily Trending Movies</h2>
+    <div class="card-container" v-if="showDaily">
+      <VueSlickCarousel :autoplay="true" :autoplaySpeed="3000">
+        <ul class="card-list">
+          <li v-for="movie in dailyTrendingMovies" :key="movie.id" class="card-item">
+            <MovieCard :movie="movie" />
+          </li>
+        </ul>
+      </VueSlickCarousel>
+    </div>
+    <h2 v-if="showWeekly">Weekly Trending Movies</h2>
+    <div class="card-container" v-if="showWeekly">
+      <VueSlickCarousel :autoplay="true" :autoplaySpeed="3000">
+        <ul class="card-list">
+          <li v-for="movie in weeklyTrendingMovies" :key="movie.id" class="card-item">
+            <MovieCard :movie="movie" />
+          </li>
+        </ul>
+      </VueSlickCarousel>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+import MovieCard from '@/components/commons/MovieCard.vue';
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+
 export default {
-    name: "TrendingList",
-}
+  name: "TrendingList",
+  components: {
+    MovieCard,
+    VueSlickCarousel
+  },
+  data() {
+    return {
+      showDaily: true,
+      showWeekly: false
+    };
+  },
+  created() {
+    this.getDailyTrendingMovies();
+    this.getWeeklyTrendingMovies();
+  },
+  methods: {
+    ...mapActions([
+      'getDailyTrendingMovies',
+      'getWeeklyTrendingMovies'
+    ]),
+    showDailyTrendingMovies() {
+      this.showDaily = true;
+      this.showWeekly = false;
+    },
+    showWeeklyTrendingMovies() {
+      this.showDaily = false;
+      this.showWeekly = true;
+    }
+  },
+  computed: {
+    ...mapState({
+      dailyTrendingMovies: state => state.trendingList.dailyTrendingMovies,
+      weeklyTrendingMovies: state => state.trendingList.weeklyTrendingMovies
+    })
+  }
+};
 </script>
 
 <style>
+.card-container {
+  overflow-x: scroll;
+}
 
+.card-list {
+  display: flex;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.card-item {
+  flex: 0 0 calc(20% - 16px);
+  margin-right: 16px;
+}
+
+.card-item:last-child {
+  margin-right: 0;
+}
+
+/* 추가한 스타일 */
+.card-item .card {
+  width: 100%;
+}
+
+.card-item .card img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
