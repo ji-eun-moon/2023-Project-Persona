d61e3cddec724323fe513b1ce6315f9c3cb670c0
문지은 <mjieun0956@gmail.com>
2023-05-19 17:35:02 +0900

    영화 검색창 생성

--- final-pjt-front/src/views/MovieView.vue
+++ final-pjt-front/src/views/MovieView.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>MovieView</h1>
+    <div class="input-group search">
+      <div>
+        <input type="text" class="form-control" placeholder="영화 제목을 입력하세요."/>
+      </div>
+      <div>
+        <i class="btn btn-light bi bi-search ms-2"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +19,19 @@ export default {
 </script>
 
 <style>
+.search {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+
+.input-group {
+  width: 100%;
+}
+
+.form-control {
+  width: 100%;
+}
 
 </style>
