76f64d2de1976226b3de686d6fb257c1c0ff593e
문지은 <mjieun0956@gmail.com>
2023-05-21 15:13:11 +0900

    좋아요 서버 구현

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
+from .models import Movie, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('user', 'movie')
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    reviews_set = ReviewSerializer(many=True, read_only=True)
+    reviews_count = serializers.IntegerField(source='reviews_set.count', read_only=True)
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
