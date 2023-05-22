80568fb45106220e08d08d630a418879cb361452
최지수 <cjjss11@naver.com>
2023-05-22 23:45:19 +0900

    게시판 절반 꾸미기..

--- final-pjt-front/src/components/community/MovieReviews.vue
+++ final-pjt-front/src/components/community/MovieReviews.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
+<div class="table-container">
+  <h5>MovieReviews</h5> 
+  <router-link :to="{ name:'CreateView' }">
+  <button>글쓰기</button>
+  </router-link>
+  
+  <div class="table-responsive">
+    <table class="table table-dark table-striped table-hover table-sm">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>제목</th>
+          <th>작성자</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ArticleListItem 
+          v-for="article in articles"
+          :key="article.id"
+          :article="article" />
+      </tbody>
+    </table>
+  </div>
+  
+</div>
+  <!-- <div>
     <h5>MovieReviews</h5>
     <router-link :to="{ name:'CreateView' }">
       <button>글쓰기</button>
     </router-link>
     <ArticleListItem 
       v-for="article in articles" :key="article.id" :article="article"/>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -27,6 +52,38 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  table {
+    width:100%;
+    border-collapse: collapse;
+    text-align: center;
+  }
+  th {
+    padding: 10px;
+    border: 1px solid #ddd;
+    background-color: #f5f5f5;
+    text-align: center;
+    color: black;
+  }
+  td {
+    padding: 10px;
+    border: 1px solid #ddd;
+    text-align: center;
+  }
+  .tr:hover {
+    background-color: #f9f9f9;
+  }
+  .title-link {
+    display: inline-block;
+    max-width: 200px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: white;
+    text-decoration: none;
+  }
+  .table-responsive {
+    max-width: 80%;
+    margin: 0 auto;
+  }
 </style>
