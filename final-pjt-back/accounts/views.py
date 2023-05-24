f0930d4d2741c040b012579d5845fe15a898021c
문지은 <mjieun0956@gmail.com>
2023-05-24 16:31:06 +0900

    좋아하는 장르 모델, 시리얼라이저, 엔드포인트 작성

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import UserSerializer, UserImgSerializer
-
+from .models import Genre
 # Create your views here.
 
 @api_view(['GET'])
@@ -31,18 +31,26 @@ def upload_image(request, username):
           serializer.save()
           return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def follow(request, username):
-    user = get_object_or_404(get_user_model(), username=username)
-    if user != request.user:
-        if user.followers.filter(pk=request.user.pk).exists():
-            user.followers.remove(request.user)
-            followed = False
-        else:
-            user.followers.add(request.user)
-            followed = True
-    context = {
-        'followed' : followed,
-    }
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def user_genres(request, username):
+    if request.method == 'GET':
+        user = get_object_or_404(get_user_model(), username=username)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method =='POST':
+        user = get_object_or_404(get_user_model(), username=username)
+        if request.user != user:
+            return Response({'profile':'권한이 없습니다.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        genre_ids = request.data.get('genre_ids', [])
+        # 기존에 저장된 장르 초기화 및 선택한 장르 추가
+        user.genres.clear()
+        for genre_id in genre_ids:
+            genre = get_object_or_404(Genre, genre_id=genre_id)
+            user.genres.add(genre)
+        
+        serializer = UserSerializer(user)  # 유저 시리얼라이저를 통해 응답 데이터 직렬화
+        return Response(serializer.data)
+    
